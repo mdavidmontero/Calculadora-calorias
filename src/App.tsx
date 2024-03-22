18187a14419208aa076b18eb59ff8f8dad3d7041
mdavidmontero <mdavidmontero6@gmail.com>
2024-03-22 18:35:37 -0500

    Agregando component para listar las actividades

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useReducer } from "react";
 import Form from "./components/Form";
+import { activityReducer, initialState } from "./reducers/activity-reducer";
+import ActivityList from "./components/ActivityList";
 
 function App() {
+  const [state, dispatch] = useReducer(activityReducer, initialState);
   return (
     <>
       <header className="bg-lime-600 py-3">
@@ -13,8 +17,11 @@ function App() {
 
       <section className="bg-lime-500 p-20 px-5 ">
         <div className="max-w-4xl mx-auto">
-          <Form />
+          <Form dispatch={dispatch} />
         </div>
+      </section>
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList activities={state.activities} />
       </section>
     </>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,22 @@
 import { categories } from "../data/categories";
-import { useState, ChangeEvent } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
 import { Activity } from "../types";
+import { ActivityActions } from "../reducers/activity-reducer";
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>;
+};
 
-export default function Form() {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0,
-  });
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+
+export default function Form({ dispatch }: FormProps) {
+  const [activity, setActivity] = useState<Activity>(initialState);
+
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
   ) => {
@@ -24,8 +33,19 @@ export default function Form() {
     return name.trim() !== "" && calories > 0;
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({
+      type: "save-activity",
+      payload: { newActivity: activity },
+    });
+    setActivity({ ...initialState, id: uuidv4() });
+  };
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">
           Categoría:
